c86e5e9ee3511daf42ce3e445d7cd96c16cc48a8
Forstandan <114364542+Forstandan@users.noreply.github.com>
2024-02-13 13:10:38 -0800

    nav bar/background img

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ResponsiveAppBar from './NavigationBar'
+// import BackgroundImage from './BackgroundImage'
+import Container from '@mui/material/Container';
+import backgroundImage from './assets/mountain_road62.jpg';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>AIot Lab</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ResponsiveAppBar /> {/* Include the ResponsiveAppBar component here */}
+      <Container 
+        maxWidth="lg"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: 'cover', // Stretch the image to cover the entire container
+          backgroundPosition: 'center',
+          height: '100vh',
+          width: '400vh',
+          padding: 0, // Remove padding
+          margin: 0, // Remove margin
+          boxSizing: 'border-box', // Include padding and border in the element's total width and height
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+        }}
+      >
+      </Container>
     </>
-  )
+  );
 }
 
 export default App
