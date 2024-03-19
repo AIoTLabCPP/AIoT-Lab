9b51e0cc08472697b0d1f9d04c3b4f1a3185d791
Nathan Wang <114364542+Forstandan@users.noreply.github.com>
2024-03-19 14:15:08 -0700

    Edit main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-"use strict";
+import React from 'react';
+import ReactDOM from 'react-dom'; // Change from 'react-dom/client' to 'react-dom'
 
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-var _App = _interopRequireDefault(require("./App.jsx"));
-require("./index.css");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_client["default"].createRoot(document.getElementById('root')).render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)));
+import App from './App'; // Assuming App.jsx is your main component
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App, null)
+  )
+);
