53c0da9be52083d0e0bff4b7aae858dfd8f2f8b5
Forstandan <114364542+Forstandan@users.noreply.github.com>
2024-03-22 18:06:19 -0700

    Update app.css and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import ResponsiveAppBar from './NavigationBar'
+import Slideshow from './Slideshow'; // Import the Slideshow component
 import Container from '@mui/material/Container';
+import team_photo from './assets/team_photo.jpg';
 
 function App() {
   const [count, setCount] = useState(0)
+  const images = [team_photo];
 
   return (
     <>
@@ -16,6 +19,18 @@ function App() {
         </div>
         <div className='main_subtext'>
           Looking forward to a better future on the road. 
+        </div>
+        <div className='project_header'>
+          WHO WE ARE
+        </div>
+        <div className='project_description'>
+          [TEMPORARY TEXT] <br />
+        </div>
+        <div className='dot_menu'>
+          &hellip;
+        </div>
+        <div className='slideshow'>
+          <Slideshow images={images}/>
         </div>
       </div>
       <Container 
