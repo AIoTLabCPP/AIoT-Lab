0be1dfc426a98172072d5b19413c2f2824194116
Nathan Wang <114364542+Forstandan@users.noreply.github.com>
2024-04-10 22:21:23 -0700

    change href

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -8,9 +8,9 @@ import aiotlogo from './assets/AIOT_logo.png';
 
 const pages = ['Outcomes', 'Team', 'News'];
 const pageLinks = [
-  './src/outcomes.html',
-  './src/team.html',
-  './src/news.html'
+  '/src/outcomes.html',
+  '/src/team.html',
+  '/src/news.html'
 ]; 
 
 function ResponsiveAppBar() {
@@ -18,7 +18,7 @@ function ResponsiveAppBar() {
     <AppBar position="fixed" style={{ top: 0, width: '100%', boxShadow: '0px 8px 4px rgba(0.5, 0.5, 0.5, 0.4)'}}> 
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <a href="./src/index.html" style={{ textDecoration: 'none', cursor: 'pointer', display: 'inline-block', position: 'fixed', left: '5vw', top: '1.2vh' }}>
+          <a href="/src/index.html" style={{ textDecoration: 'none', cursor: 'pointer', display: 'inline-block', position: 'fixed', left: '5vw', top: '1.2vh' }}>
             <img alt="AIOT Logo" src={aiotlogo} style={{ width: 110, height: 40, marginRight: 8}} />
           </a>
           
