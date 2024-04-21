92ff8805923cf654ac2a49cab62a19c36b60c112
Nathan Wang <114364542+Forstandan@users.noreply.github.com>
2024-04-21 15:58:26 -0700

    Modify routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom';
-// import Team from './team.jsx'
 import './index.css'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Route,
-// } from "react-router-dom"
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App/>,
-//   },
-//   {
-//     path: "team",
-//     element: <Team/>,
-//   }
-// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
